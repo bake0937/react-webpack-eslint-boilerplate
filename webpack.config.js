d0112ed29e3096b2ff19b5ef32eaf42054fdd459
bake0937 <sadayasu.info@gmail.com>
2018-05-18 08:03:25 +0900

    Loaderの設定を追加

--- webpack.config.js
+++ webpack.config.js
@@ -6,5 +6,26 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist/js'),
     filename: 'bundle.js'
-  }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['babel-preset-env', 'react']
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.css']
+  },
 };
